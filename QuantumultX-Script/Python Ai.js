48b94ebf941d75583247bb2b022ee2847a412046
SeanWang1224 <www.wangxiang5219@outlook.com>
2021-03-14 15:55:49 +0800

    Update Python Ai.js

--- QuantumultX-Script/Python Ai.js
+++ QuantumultX-Script/Python Ai.js
@@ -2,11 +2,13 @@
 圈X
 [rewrite_local]
 #Python Ai解锁会员
-^http:\/\/ws\.60he\.com\/(book|user).+ url script-response-body https://raw.githubusercontent.com/SeanWang1224/QuantumultX/master/QuantumultX-Script/Python Ai.js
+^http:\/\/ws\.60he\.com\/(book|user).+ url script-response-body https://raw.githubusercontent.com/SeanWang1224/QuantumultX/master/QuantumultX-Script/Python%20Ai.js
 [mitm]
 hostname= ws.60he.com
+
 下载地址：
 https://apps.apple.com/cn/app/id1471351733
+
 */
 
 
