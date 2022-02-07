b21927e1bb7ca6c0161a92ad9cd387aff8ebfd5b
SeanWang1224 <www.wangxiang5219@outlook.com>
2022-02-07 21:51:26 +0800

    Update Varlens.js

--- QuantumultX-Script/Varlens.js
+++ QuantumultX-Script/Varlens.js
@@ -2,8 +2,7 @@
 QX:
 [rewrite_local]
 #Varlens解锁PRO
-^https:\/\/mid\.zineapi\.com\/@varlens\/api\/user\/info\/ url script-response-body https://raw.githubusercontent.com/SeanWang1224/QuantumultX/master/QuantumultX-Script/Varlens.js
-
+^https?:\/\/mid\.zineapi\.com\/@varlens\/api\/user\/info\/ url script-response-body https://github.com/ddgksf2013/Cuttlefish/raw/master/Crack/varlens.js
 [mitm]
 hostname = mid.zineapi.com
 
@@ -12,25 +11,7 @@ https://apps.apple.com/cn/app/id1535205062
 
 */
 
-let obj = JSON.parse($response.body);
-obj = {
-  "success": true,
-  "data": {
-    "username": "ioshkj",
-    "app_name": "varlens",
-    "nick": null,
-    "email": "",
-    "status": 0,
-    "preferences": null,
-    "membership": {
-      "role_name": "pro",
-      "begin_time": "2021-03-13T09:59:11.485Z",
-      "end_time": "2045-12- 31T00:00:00.481Z",
-      "begin_time_ms": 1615629551485,
-      "end_time_ms": 2398327914000
-    },
-    "social_apple_id": null
-  }
-}
+var ddgksf2013 = 
+{"success":true,"data":{"username":"dustudent","app_name":"dustudent","nick":null,"email":"","status":0,"preferences":null,"membership":{"role_name":"pro","begin_time":"2021-03-13T09:59:11.485Z","end_time":"2099-12-31T00:00:00.481Z","begin_time_ms":1615629551485,"end_time_ms":4102325303000},"social_apple_id":null}};
 
-$done({body: JSON.stringify(obj)});
+$done({body : JSON.stringify(ddgksf2013)});
